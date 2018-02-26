65d767752dea0fc3d491f1a2868023c2449d9ca0
Ashish Narasimham <ashish.narasimham@gmail.com>
2018-02-26 16:18:11 -0500

    added hive support for insert scripts pos/manufacturing

--- inserter/main_manf.py
+++ inserter/main_manf.py
@@ -1,4 +1,5 @@
 from insert_data import MySQLInserter
+from insert_data import HiveInserter
 import sys
 import time
 import random
@@ -6,14 +7,28 @@ import random
 sys.path.append('../datagen')
 from datagen import ManufacturingDataGenerator
 
+if len(sys.argv) < 3:
+    print('Please provide 2 arguments, first is number of records, second is hive or mysql for db to insert into')
+    sys.exit(1)
+
 num_rows = int(sys.argv[1])
+hive_or_mysql = sys.argv[2]
 
-f = open('mysql.passwd')
+if hive_or_mysql == 'hive':
+    filename = 'hive.passwd'
+else:
+    filename = 'mysql.passwd'
 
+f = open(filename)
 host,port,username,password,db,table = f.read().splitlines()
 
-ins = MySQLInserter(host, port, username, password, db, table , 
-        ['time','id','shortname','notes','part_loc','vibr_tolr_pct','vibr_tolr_thrs','heat_tolr_pct','heat_tolr_thrs','qty'])
+if hive_or_mysql == 'hive':
+    ins = HiveInserter(host, port, username, password, db, table ,
+            ['time','id','shortname','notes','part_loc','vibr_tolr_pct','vibr_tolr_thrs','heat_tolr_pct','heat_tolr_thrs','qty'])
+else:
+    ins = MySQLInserter(host, port, username, password, db, table ,
+            ['time','id','shortname','notes','part_loc','vibr_tolr_pct','vibr_tolr_thrs','heat_tolr_pct','heat_tolr_thrs','qty'])
+
 gen = ManufacturingDataGenerator()
 
 rows = []
--- inserter/main_pos.py
+++ inserter/main_pos.py
@@ -1,4 +1,5 @@
 from insert_data import MySQLInserter
+from insert_data import HiveInserter
 import sys
 import time
 import random
@@ -6,14 +7,27 @@ import random
 sys.path.append('../datagen')
 from datagen import POSDataGenerator
 
-num_rows = int(sys.argv[1])
+if len(sys.argv) < 3:
+    print('Please provide 2 arguments, first is number of records, second is hive or mysql for db to insert into')
+    sys.exit(1)
 
-f = open('mysql.passwd')
+hive_or_mysql = sys.argv[2]
 
+if hive_or_mysql == 'hive':
+    filename = 'hive.passwd'
+else:
+    filename = 'mysql.passwd'
+
+f = open(filename)
 host,port,username,password,db,table = f.read().splitlines()
 
-ins = MySQLInserter(host, port, username, password, db, table , 
-        ['id','trxn_time','cust_id','trxn_amt','discount_amt','store_id','rep_id','part_sku','qty'])
+if hive_or_mysql == 'hive':
+    ins = HiveInserter(host, port, username, password, db, table , 
+            ['id','trxn_time','cust_id','trxn_amt','discount_amt','store_id','rep_id','part_sku','qty'])
+else:
+    ins = MySQLInserter(host, port, username, password, db, table , 
+            ['id','trxn_time','cust_id','trxn_amt','discount_amt','store_id','rep_id','part_sku','qty'])
+
 gen = POSDataGenerator()
 
 rows = []
